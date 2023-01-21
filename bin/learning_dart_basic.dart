88c51fbe5647b5e351a2fc41929209f0ce117279
Ramenaru <ramzieinstein00@gmail.com>
2023-01-21 21:00:31 +0700

    dart commit

---
+++ bin/learning_dart_basic.dart
@@ -0,0 +1,5 @@
+import 'package:learning_dart_basic/learning_dart_basic.dart' as learning_dart_basic;
+
+void main(List<String> arguments) {
+  print('Hello world: ${learning_dart_basic.calculate()}!');
+}
