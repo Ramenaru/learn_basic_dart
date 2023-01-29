08ad30ef2cb5ee358b719aad7d8cc109bd4f7bb4
Ramenaru <ramzieinstein00@gmail.com>
2023-01-29 09:01:20 +0700

    boolean data types

---
+++ bin/learn_4.dart
@@ -0,0 +1,19 @@
+void main() {
+  //there is 2 type of bool or boolean which is true and false
+
+  //true
+  bool theTrue = true;
+  print(theTrue);
+
+  //false
+  bool theFalse = false;
+  print(theFalse);
+
+  num number = 30;
+
+  if (number == theFalse) {
+    print("Wrong Number");
+  } else {
+    print("Right Number");
+  }
+}
