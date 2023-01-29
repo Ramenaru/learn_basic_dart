d31ae2ea295bebe631311df67e090c1f5b1447b2
Ramenaru <ramzieinstein00@gmail.com>
2023-01-29 08:54:41 +0700

    number data types

---
+++ bin/learn_3.dart
@@ -0,0 +1,15 @@
+void main() {
+  //there is 2 type of number (int, double)
+  //int for non decimal number
+  //double for decimal number (using '.' not ',')
+
+  int number1 = 10;
+  double number2 = 20.5;
+
+  print(number1 + number2);
+
+  //num data type
+  num theNumber = 30.6;
+  num theNumber2 = 30;
+  print(theNumber);
+}
