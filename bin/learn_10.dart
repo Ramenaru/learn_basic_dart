a3c1a6f6eb528b5999c6d6dca7028edfda6f2e78
Ramenaru <ramzieinstein00@gmail.com>
2023-01-30 14:38:41 +0700

    assignment operators

---
+++ bin/learn_10.dart
@@ -0,0 +1,24 @@
+//assignment operator
+
+void main() {
+  var a = 10;
+
+  //the difference between arithmetic Operators and assignment
+  print(a = a + 10);
+  print(a = a - 10);
+  print(a = a * 10);
+  print(a += 10);
+  print(a -= 10);
+  print(a *= 10);
+
+  var i = 10;
+  // i++;
+  print(i);
+
+  var g = i++;
+  print(g);
+
+  var k = ++i;
+
+  print(++i);
+}
