23361c6dbd767778b4b42941ba2d1316b15ccb8e
Ramenaru <ramzieinstein00@gmail.com>
2023-01-30 13:57:38 +0700

    arithmetic operation

---
+++ bin/learn_8.dart
@@ -0,0 +1,21 @@
+//arithmetics operator like +, *, /
+
+void main() {
+  var resultsPlus = 66 + 66;
+  var resultsMin = 66 - 66;
+  var resultsNegative = -66;
+  var resultsPositive = 66;
+  var resultsTimes = 66 * 66;
+  var resultsFor = 66 / 66;
+  var resultsForButInt = 66 ~/ 66;
+  var resultsModulo = 66 % 66;
+
+  print(resultsPlus);
+  print(resultsMin);
+  print(resultsNegative);
+  print(resultsPositive);
+  print(resultsTimes);
+  print(resultsFor);
+  print(resultsForButInt);
+  print(resultsModulo);
+}
