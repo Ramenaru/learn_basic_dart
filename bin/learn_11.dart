ac5c455e4b36bc475376b4cb19b13b86c3bb78fd
Ramenaru <ramzieinstein00@gmail.com>
2023-01-30 15:16:14 +0700

    logic operator

---
+++ bin/learn_11.dart
@@ -0,0 +1,32 @@
+//logic operator
+
+//&& and
+// II is or
+
+//II = at least one true it is true
+//&& = must be true twice
+
+void main() {
+  var bestValue = 90;
+  var currentValue = 5;
+  var minimumValue = 75;
+
+  //if you want to succes the test, minimum value is 75
+  var toSuccess = minimumValue;
+  toSuccess == bestValue;
+  toSuccess == currentValue;
+
+  var wantToSuccess = minimumValue >= minimumValue;
+  print(wantToSuccess);
+
+  var youSucces = currentValue == wantToSuccess;
+  if (youSucces == true) {
+    print('your success budy!');
+  } else {
+    print('your fail');
+  }
+
+  //it is false and true
+  print(!true); //false
+  print(!false); //true
+}
