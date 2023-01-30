ad489d7d2a4ed72bad8381b170b57b8a3085617c
Ramenaru <ramzieinstein00@gmail.com>
2023-01-30 15:34:11 +0700

    type test operators

---
+++ bin/learn_12.dart
@@ -0,0 +1,18 @@
+//type test operator
+
+void main() {
+  int num = 10;
+
+  // var convertNumber = num as double;
+  // print(convertNumber);
+
+  //if you trying to code like this it is error
+  //should be using 'is' first like,
+  var convertNumber2 = num is double; //?
+  print(convertNumber2);
+  //system say false and then there is
+  var convertNumber3 = num is int;
+  print(convertNumber3); //and then they say true ok
+  var convertNumber4 = num as int;
+  print(convertNumber4);
+}
