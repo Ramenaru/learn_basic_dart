d747fe653b0e5ca0e03b2c858b239ecd412d0e71
Ramenaru <ramzieinstein00@gmail.com>
2023-01-30 09:52:27 +0700

    dynamic data types

---
+++ bin/learn_6.dart
@@ -0,0 +1,19 @@
+//dynamic data types
+//is used to accommodate any data even numeric and string together
+
+void main() {
+  dynamic number = 100;
+  number = 'number';
+  number = true;
+  number = 100.5;
+
+  /*
+  if you use var as not declare like 'var variable;' 
+  it is automatically read as dynamic
+  */
+  var dynamics;
+  dynamics = 10;
+  dynamics = 10.5;
+  dynamics = '10.5';
+  dynamics = true;
+}
