12946178560e1316ea459404f21013347df3c9dc
Ramenaru <ramzieinstein00@gmail.com>
2023-01-30 14:06:55 +0700

    comparsion operator

---
+++ bin/learn_9.dart
@@ -0,0 +1,13 @@
+//comparsion operators
+
+void main() {
+  //here is the list
+  // '==' '!=' '<' '>' '>=' '<='
+
+  print(10 == 10); //true
+  print(10 != 10); //flase
+  print(10 < 10); //false beacuse it is equally
+  print(10 > 10); //false beacuse it is equally
+  print(10 >= 10); //true
+  print(10 <= 10); //true
+}
