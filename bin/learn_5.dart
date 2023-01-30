f0a1ef5c711c6184c315d10a9ac4b1aea06cbb8e
Ramenaru <ramzieinstein00@gmail.com>
2023-01-30 08:41:20 +0700

    the string dude

---
+++ bin/learn_5.dart
@@ -0,0 +1,11 @@
+//This part is part of string
+void main() {
+  //that is string
+  print("This is String");
+
+  //using variable
+  String myString = "Hello my name is string man";
+  print(myString);
+
+  //========String Interpolation========//
+}
