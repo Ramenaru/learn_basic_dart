b513a66df642cc5d3babc9309d21bbaa7bcf2f9a
Ramenaru <ramzieinstein00@gmail.com>
2023-01-31 08:51:44 +0700

    switch case

---
+++ bin/learn_18.dart
@@ -0,0 +1,22 @@
+//this is switch case dude
+
+void main() {
+  var nilai = '100';
+  switch (nilai) {
+    case '100':
+      print('You are wonderfull');
+      break;
+    case '90':
+    case '80':
+      print('oke you are good');
+      break;
+    case '75':
+      print('oke thats something problem here');
+      break;
+    case '60':
+      print('umm sorry you must do a remedy stuff');
+      break;
+    default:
+      print('you are fail');
+  }
+}
