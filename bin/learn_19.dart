9195fd84adcd1b32a2562df647d9ae99bec0035d
Ramenaru <ramzieinstein00@gmail.com>
2023-01-31 08:57:38 +0700

    null types

---
+++ bin/learn_19.dart
@@ -0,0 +1,11 @@
+//null type in dart
+
+void main() {
+  int number1;
+  int? number2;
+
+  print(number2);
+
+  //if you want to nullable some data types in your program
+  //null is the choice for it
+}
