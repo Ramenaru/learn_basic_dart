47dd53084de93844c85abe753e46f6d2da3b1698
Ramenaru <ramzieinstein00@gmail.com>
2023-01-31 09:33:20 +0700

    for loop

---
+++ bin/learn_22.dart
@@ -0,0 +1,13 @@
+//for loop
+
+// void main() {
+//   for (;;) {
+//     print('diulang');
+//   }
+// } endless loop
+
+void main() {
+  for (var counter = 3; counter <= 10; counter++) {
+    print('perulangan ke-$counter');
+  }
+}
