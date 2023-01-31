6c2d38a30218e65a2e39c2f79e8ec3939136a61d
Ramenaru <ramzieinstein00@gmail.com>
2023-01-31 16:27:56 +0700

    learn_31.dart

---
+++ bin/learn_31.dart
@@ -0,0 +1,13 @@
+//function short expression
+
+int sum(int first, int second) => first + second;
+
+void main() {
+  print(sum(10, 11));
+  print(sum(10, 11));
+
+  var total = sum(10, 10);
+  print(total);
+
+  print(sum(5, 5));
+}
